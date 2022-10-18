9b7e4c4e6ee4cfb20c11a92b2d4d0a187b7cfd2c
Dantevg <dantevg006@gmail.com>
2022-10-18 17:25:57 +0200

    Add combined test cases and multi-command test support

--- run_tests.py
+++ run_tests.py
@@ -5,14 +5,18 @@ import server_client
 
 def get_commands(device):
 	test_commands = {
-		"baseline": "",
-		"delay": f"sudo tc qdisc add dev {device} root netem delay 200ms",
-		"jittered delay": f"sudo tc qdisc add dev {device} root netem delay 200ms 75ms",
-		"packet drops": f"sudo tc qdisc add dev {device} root netem loss 20%",
-		"low throughput": f"sudo tc qdisc add dev {device} root tbf rate 256kbit burst 16kbit latency 400ms",
-		"packet reordering": f"sudo tc qdisc add dev {device} root netem delay 10ms reorder 25% 50%",
-		"packet duplication": f"sudo tc qdisc add dev {device} root netem duplicate 5%",
-		"bitflips": f"sudo tc qdisc add dev {device} root netem corrupt 10%",
+		"baseline": [],
+		"delay": [f"sudo tc qdisc add dev {device} root netem delay 200ms"],
+		"jittered delay": [f"sudo tc qdisc add dev {device} root netem delay 200ms 75ms"],
+		"packet drops": [f"sudo tc qdisc add dev {device} root netem loss 20%"],
+		"low throughput": [f"sudo tc qdisc add dev {device} root tbf rate 256kbit burst 16kbit latency 400ms"],
+		"packet reordering": [f"sudo tc qdisc add dev {device} root netem delay 10ms reorder 25% 50%"],
+		"packet duplication": [f"sudo tc qdisc add dev {device} root netem duplicate 5%"],
+		"bitflips": [f"sudo tc qdisc add dev {device} root netem corrupt 10%"],
+		"packet reordering & packet duplication": [f"sudo tc qdisc add dev {device} root netem delay 10ms reorder 25% 50% duplicate 5%"],
+		# TODO: fix the second command, which now errors with "Error: Invalid qdisc name."
+		"packet drops & low throughput": [f"sudo tc qdisc add dev {device} root netem loss 20%", f"sudo tc qdisc change dev {device} root tbf rate 256kbit burst 16kbit latency 400ms"],
+		"packet duplication & bitflips": [f"sudo tc qdisc add dev {device} root netem duplicate 5% corrupt 10%"],
 	}
 	
 	reset_command = f"sudo tc qdisc del dev {device} root"
@@ -22,11 +26,15 @@ def exec_command(command):
 	print(f"test: running command '{command}'")
 	return os.system(command)
 
+def exec_commands(commands):
+	return all([exec_command(c) == 0 for c in commands])
+
 def run_test(desc, commands):
 	(test_commands, reset_command) = commands
 	print(f"\n===== test '{desc}'")
-	if exec_command(test_commands[desc]) != 0:
+	if not exec_commands(test_commands[desc]):
 		print("Failed to execute network tampering command")
+		exec_command(reset_command)
 		return
 	if desc == "delay" or desc == "jittered delay":
 		server_client.run_server_client(client_args={"messages": 10, "update_interval": 1})
@@ -42,7 +50,7 @@ def main():
 	(test_commands, reset_command) = get_commands(sys.argv[1])
 	
 	if os.geteuid() != 0:
-		print("tc command need root, run again with sudo")
+		print("tc command needs root, run again with sudo")
 		return
 	
 	try:
