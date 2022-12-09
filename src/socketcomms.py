7d9d74b7dafdbb2588c96f0ca7d75c692546a7c3
RedPolygon <dantevg006@gmail.com>
2022-12-09 12:22:40 +0100

    Ch ch ch changeees

--- src/socketcomms.py
+++ src/socketcomms.py
@@ -1,21 +1,26 @@
 import sys
 import socket
 
+# krijgt een packetlijst en een lijst van tamperings en moet als output de packetlijst van torxakis teruggeven.
+
 def main():
-    if len(sys.argv) != 1:
+    if len(sys.argv) != 2:
         print("Port number required")
         return
     host = socket.gethostname()
-    portnr = sys.argv[0]
+    portnr = int(sys.argv[1])
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, portnr))
         print("waiting for Tester")
         s.listen()
         conn, address = s.accept()
+        print("Tester connected")
         with conn:
             while True:
-                data = conn.recv(1024)
+                data = conn.recv(1024).decode()
+                print(data)
+
 
 
 if __name__ == "__main__":
