6484ff396515820cb03684f8046f6a143c67d52d
julius <juliuslandsman@hotmail.com>
2022-12-12 20:40:56 +0100

    Added input parsing

--- src/socketcomms.py
+++ src/socketcomms.py
@@ -3,15 +3,36 @@ import socket
 
 # krijgt een packetlijst en een lijst van tamperings en moet als output de packetlijst van torxakis teruggeven.
 
+def find_all(i_str, substr):
+    start = 0
+    while True:
+        start = i_str.find(substr, start)
+        if start == -1: return
+        yield start
+        start += len(substr)
+
+def process_input(data):
+    tamp_ind = data.find("))")+1
+    tamp_input = data[37:tamp_ind]
+    sep = tamp_input.find(')')
+    tamp1 = tamp_input[:sep+1]
+    tamp2 = tamp_input[sep+2:]
+    packet_ind1 = list(find_all(data, '(\"'))
+    packet_ind2 = list(find_all(data, '\",Packet'))
+    packet_data_list = []
+    for i in range(len(packet_ind1)):
+        packet_data = data[packet_ind1[i]+2:packet_ind2[i]]
+        packet_data_list.append(packet_data)
+    return tamp1, tamp2, packet_data_list
+
 def main():
     if len(sys.argv) != 2:
         print("Port number required")
         return
-    host = socket.gethostname()
     portnr = int(sys.argv[1])
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, portnr))
+        s.bind(("localhost", portnr))
         print("waiting for Tester")
         s.listen()
         conn, address = s.accept()
@@ -19,7 +40,8 @@ def main():
         with conn:
             while True:
                 data = conn.recv(1024).decode()
-                print(data)
+                tamp1, tamp2, packet_data = process_input(data)
+                
 
 
 
