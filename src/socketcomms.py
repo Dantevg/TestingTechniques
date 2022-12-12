dcc93d6812542ef285d8bccb16a85980517256ae
julius <juliuslandsman@hotmail.com>
2022-12-12 21:26:57 +0100

    Updated parser

--- src/socketcomms.py
+++ src/socketcomms.py
@@ -11,19 +11,35 @@ def find_all(i_str, substr):
         yield start
         start += len(substr)
 
+def parse_tampering(tamp):
+    tamp_spl = tamp.split('(')
+    args_ind = list(find_all(tamp_spl[1], ','))
+    if not args_ind:
+        args = [int(tamp_spl[1][:-1])]
+    else:
+        args = [int(tamp_spl[1][:args_ind[0]])]
+        for i in range(len(args_ind)):
+            if i+1 == len(args_ind):
+                args.append(int(tamp_spl[1][args_ind[i]+1:-1]))
+            else:
+                args.append(int(tamp_spl[1][args_ind[i]+1:args_ind[i+1]]))
+    return (tamp_spl[0], args)
+
 def process_input(data):
     tamp_ind = data.find("))")+1
     tamp_input = data[37:tamp_ind]
     sep = tamp_input.find(')')
     tamp1 = tamp_input[:sep+1]
     tamp2 = tamp_input[sep+2:]
+    tamp1_parsed = parse_tampering(tamp1)
+    tamp2_parsed = parse_tampering(tamp2)
     packet_ind1 = list(find_all(data, '(\"'))
     packet_ind2 = list(find_all(data, '\",Packet'))
     packet_data_list = []
     for i in range(len(packet_ind1)):
         packet_data = data[packet_ind1[i]+2:packet_ind2[i]]
         packet_data_list.append(packet_data)
-    return tamp1, tamp2, packet_data_list
+    return tamp1_parsed, tamp2_parsed, packet_data_list
 
 def main():
     if len(sys.argv) != 2:
@@ -40,8 +56,8 @@ def main():
         with conn:
             while True:
                 data = conn.recv(1024).decode()
-                tamp1, tamp2, packet_data = process_input(data)
-                
+                (tamp1, args1), (tamp2, args2), packet_data = process_input(data)
+
 
 
 
