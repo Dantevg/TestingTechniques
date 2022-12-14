d8d6af7cffa8c2b6651b5bf474c65ce6ff28d66c
julius <juliuslandsman@hotmail.com>
2022-12-14 14:25:15 +0100

    Added package sending with tampering

---
+++ src/server_client2.py
@@ -0,0 +1,48 @@
+import queue
+import socket
+from threading import Thread
+import os
+import time
+
+def server(host = "127.0.0.1", port=8002):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((host, port))
+        s.listen()
+        conn, addr = s.accept()
+        print(f"server: started on {host}:{port}")
+        with conn:
+            print(f"server: connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+def client(
+    q,
+    host = "127.0.0.1", 
+    port=8002, 
+    messages = []):
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        print(f"client: connected to {host}:{port}")
+
+        for m in messages:
+            s.sendall(m.encode())
+            data = s.recv(1024).decode()
+            q.put(data)
+
+def run_server_client(server_args = None, client_args = None):
+    q = queue.LifoQueue()
+    server_thread = Thread(target=server, kwargs=server_args)
+    client_thread = Thread(target=client, args=(q,), kwargs=client_args)
+    server_thread.start()
+    client_thread.start()
+    server_thread.join()
+    client_thread.join()
+    return q
+
+if __name__ == "__main__":
+    run_server_client()
