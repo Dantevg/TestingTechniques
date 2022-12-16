eed0bc33da4632c2b4d8c0c81b91ffc479d99b18
julius <juliuslandsman@hotmail.com>
2022-12-16 12:37:38 +0100

    Fixed torxakis

--- src/socketcomms.py
+++ src/socketcomms.py
@@ -19,48 +19,65 @@ def find_all(i_str, substr):
         start += len(substr)
 
 
-def get_command(tamp, args):
-    args_len = len(args)
-    if args_len == 0:
-        return ""
-    elif args_len == 1:
-        if tamp == "Delay":
-            return f"sudo tc qdisc add dev lo root netem delay {args[0]}ms"
-        elif tamp == "PacketDrops":
-            return f"sudo tc qdisc add dev lo root netem loss {args[0]}%"
-        elif tamp == "Duplication":
-            return f"sudo tc qdisc add dev lo root netem duplicate {args[0]}%"
-        else:
-            return f"sudo tc qdisc add dev lo root netem corrupt {args[0]}%"
-    elif args_len == 2:
-        if tamp == "JitteredDelay":
-            return f"sudo tc qdisc add dev lo root netem delay {args[0]}ms {args[1]}ms"
-        else:
-            return f"sudo tc qdisc add dev lo root netem delay 10ms reorder {args[0]}% {args[1]}%"
-    else:
-        return f"sudo tc qdisc add dev lo root tbf rate {args[0]}kbit burst {args[1]}kbit latency {args[2]}ms"
+def get_command(tamp1, args1, tamp2, args2):
+    command = "sudo tc qdisc add dev lo root netem"
+    if tamp1 == "Delay":
+        command = f"{command} delay {args1[0]}ms"
+    if tamp2 == "Delay":
+        command = f"{command} delay {args2[0]}ms"
+    if tamp1 == "JitteredDelay":
+        command = f"{command} delay {args1[0]}ms {args1[1]}ms"
+    if tamp2 == "JitteredDelay":
+        command = f"{command} delay {args2[0]}ms {args2[1]}ms"
+    if tamp1 == "Reordering":
+        command = f"{command} delay 10ms reorder {args1[0]}% {args1[1]}%"
+    if tamp2 == "Reordering":
+        command = f"{command} delay 10ms reorder {args2[0]}% {args2[1]}%"
+    if tamp1 == "PacketDrops":
+        command = f"{command} loss {args1[0]}%"
+    if tamp2 == "PacketDrops":
+        command = f"{command} loss {args2[0]}%"
+    if tamp1 == "Duplication":
+        command = f"{command} duplicate {args1[0]}%"
+    if tamp2 == "Duplication":
+        command = f"{command} duplicate {args2[0]}%"
+    if tamp1 == "Bitflips":
+        command = f"{command} corrupt {args1[0]}%"
+    if tamp2 == "Bitflips":
+        command = f"{command} corrupt {args2[0]}%"
+    if tamp1 == "LowThroughput":
+        command = f"{command} rate {args1[0]}kbit latency {args1[2]}ms"
+    if tamp2 == "LowThroughput":
+        command = f"{command} rate {args2[0]}kbit latency {args2[2]}ms"
+    return command
 
 
 def parse_tampering(tamp):
-    tamp_spl = tamp.split('(')
-    args_ind = list(find_all(tamp_spl[1], ','))
-    if not args_ind:
-        args = [int(tamp_spl[1][:-1])]
+    if tamp == "None":
+        return ("None", [])
     else:
-        args = [int(tamp_spl[1][:args_ind[0]])]
-        for i in range(len(args_ind)):
-            if i+1 == len(args_ind):
-                args.append(int(tamp_spl[1][args_ind[i]+1:-1]))
-            else:
-                args.append(int(tamp_spl[1][args_ind[i]+1:args_ind[i+1]]))
-    return (tamp_spl[0], args)
+        tamp_spl = tamp.split('(')
+        args_ind = list(find_all(tamp_spl[1], ','))
+        if not args_ind:
+            args = [int(tamp_spl[1][:-1])]
+        else:
+            args = [int(tamp_spl[1][:args_ind[0]])]
+            for i in range(len(args_ind)):
+                if i+1 == len(args_ind):
+                    args.append(int(tamp_spl[1][args_ind[i]+1:-1]))
+                else:
+                    args.append(int(tamp_spl[1][args_ind[i]+1:args_ind[i+1]]))
+        return (tamp_spl[0], args)
 
 def process_input(data):
-    tamp_ind = data.find("))")+1
+    tamp_ind = data.find("),Packet_cons")
     tamp_input = data[37:tamp_ind]
-    sep = tamp_input.find(')')
-    tamp1 = tamp_input[:sep+1]
-    tamp2 = tamp_input[sep+2:]
+    if tamp_input.startswith('None'):
+        sep = tamp_input.find(',')
+    else:
+        sep = tamp_input.find(')')+1
+    tamp1 = tamp_input[:sep]
+    tamp2 = tamp_input[sep+1:]
     tamp1_parsed = parse_tampering(tamp1)
     tamp2_parsed = parse_tampering(tamp2)
     packet_ind1 = list(find_all(data, '(\"'))
@@ -76,6 +93,7 @@ def send_packages(tampering_cmd, packet_data):
         print("Failed to execute network tampering command")
         exec_command("sudo tc qdisc del dev lo root")
         return
+    return_data = queue.LifoQueue()
     return_data = server_client2.run_server_client(client_args={"messages": packet_data})
     exec_command("sudo tc qdisc del dev lo root")
     return return_data
@@ -103,8 +121,7 @@ def main():
             while True:
                 data = conn.recv(1024).decode()
                 (tamp1, args1), (tamp2, args2), packet_data = process_input(data)
-                tampering_cmd = get_command(tamp1, args1)
-                return_data_q = queue.LifoQueue
+                tampering_cmd = get_command(tamp1, args1, tamp2, args2)
                 return_data_q = send_packages(tampering_cmd, packet_data)
                 return_string = build_return_string(return_data_q)
                 conn.send(return_string.encode())
